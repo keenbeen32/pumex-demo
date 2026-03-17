e27030ada4d11a9bf50075f885554b6b2f35569c
keenbeen32 <kenauvith@gmail.com>
2026-03-17 12:52:25 +0200

    deploy push

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -264,7 +264,7 @@ VoterV5.GaugeCreated.handler(async ({ event, context }) => {
     lastUpdatedBlock: BigInt(event.block.number),
   };
 
-  context.GaugeStats.set(gaugeStats);
+  context.GaugeStats.set(gaugeStats); 
 });
 
 VoterV5.GaugeKilled.handler(async ({ event, context }) => {
